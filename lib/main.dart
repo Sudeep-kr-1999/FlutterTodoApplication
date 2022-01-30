7652190e77f607216953b897a2ae795a4c1dde54
Sudeep Kumar <sudeepagrawal1999@gmail.com>
2022-01-30 23:07:35 +0530

    design modified and layout changes made

--- lib/main.dart
+++ lib/main.dart
@@ -31,23 +31,33 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        // centerTitle: true,
+        centerTitle: true,
         title: const Text("Track Your Plans"),
-        backgroundColor: Colors.black,
+        backgroundColor: Colors.deepPurple,
+        elevation: 5,
+        shadowColor: Colors.white,
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Padding(
-              padding: EdgeInsets.fromLTRB(12, 0, 0, 8),
-              child: Text(
-                "Active Tasks",
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  fontStyle: FontStyle.normal,
+            Card(
+              margin: const EdgeInsets.fromLTRB(15, 10, 10, 0),
+              color: Colors.purple,
+              elevation: 5,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20)),
+              child: const Padding(
+                padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                child: Text(
+                  "Active Tasks",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.normal,
+                    color: Colors.white,
+                  ),
                 ),
               ),
             ),
@@ -57,15 +67,13 @@ class MyHomePage extends StatelessWidget {
             Expanded(
               child: Scrollbar(
                 scrollbarOrientation: ScrollbarOrientation.right,
+                isAlwaysShown: true,
+                thickness: 5,
+                radius: const Radius.circular(50),
+                interactive: true,
                 child: ListView(
                   children: const [
                     ActivityCard("Pay Fees", "21-01-2021", "will pay the fees"),
-                    ActivityCard("Pay Bill", "21-01-2021", "will pay bills"),
-                    ActivityCard(
-                        "Do Recharge", "21-01-2021", "will do recharge"),
-                    ActivityCard("Pay Bill", "21-01-2021", "will pay bills"),
-                    ActivityCard("Pay Bill", "21-01-2021", "will pay bills"),
-                    ActivityCard("Pay Bill", "21-01-2021", "will pay bills"),
                   ],
                 ),
               ),
@@ -75,9 +83,9 @@ class MyHomePage extends StatelessWidget {
       ),
       floatingActionButton: FloatingActionButton(
         foregroundColor: Colors.white,
-        backgroundColor: Colors.black,
+        backgroundColor: Colors.purple,
         elevation: 5,
-        splashColor: Colors.green,
+        splashColor: Colors.grey,
         onPressed: () {
           // HERE WE HAVE TO PUSH SCREEN RIGHT HERE
           // Navigator.push(context,
@@ -104,46 +112,49 @@ class ActivityCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-        elevation: 5,
-        borderOnForeground: false,
-        shadowColor: Colors.black,
-        color: Colors.white,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
-        child: Container(
-          padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
-          width: double.infinity,
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 20,
-                width: 20,
-                // checkbox execution in statefull widget
-                child: Checkbox(value: false, onChanged: (bool? value) {}),
-              ),
-              // simply a empty container
-              const SizedBox(
-                width: 30,
-                height: 30,
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    header!,
-                    style: const TextStyle(
-                      color: Colors.black,
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Card(
+          elevation: 5,
+          borderOnForeground: true,
+          color: Colors.white,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+          child: Container(
+            padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+            width: double.infinity,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 50,
+                  width: 30,
+                  // checkbox execution in statefull widget
+                  child: Checkbox(value: false, onChanged: (bool? value) {}),
+                ),
+                // simply a empty container
+                const SizedBox(
+                  width: 20,
+                  height: 30,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      header!,
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                  Text(date!),
-                  Text(list!),
-                ],
-              )
-            ],
-          ),
-        ));
+                    Text(date!),
+                    Text(list!),
+                  ],
+                )
+              ],
+            ),
+          )),
+    );
   }
 }
--- lib/screens/newtaskscreen.dart
+++ lib/screens/newtaskscreen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:todoapplication/CustomIcon.dart';
 
 class NewTaskScreen extends StatefulWidget {
   const NewTaskScreen({Key? key}) : super(key: key);
@@ -16,6 +15,7 @@ class _NewTaskScreenState extends State<NewTaskScreen> {
   final TextEditingController _dateController = TextEditingController();
   final TextEditingController _timeController = TextEditingController();
   String repetitionFrequency = "No Repeat";
+
   List<String> options = [
     "No Repeat",
     "Once a Day",
@@ -23,6 +23,7 @@ class _NewTaskScreenState extends State<NewTaskScreen> {
     "Once a Week",
     "Other"
   ];
+
   List<DropdownMenuItem<String>>? dropDownItemCreator(List<String> menuItem) {
     List<DropdownMenuItem<String>>? dropDownMenuItems = menuItem
         .map((item) =>
@@ -35,197 +36,243 @@ class _NewTaskScreenState extends State<NewTaskScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+      floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.deepPurple,
+          foregroundColor: Colors.white,
+          splashColor: Colors.grey,
+          elevation: 5,
+          child: const Icon(Icons.check),
+          onPressed: () {
+            print("data saved to database");
+          }),
       appBar: AppBar(
+        centerTitle: true,
         title: const Text("Create Your Task"),
-        backgroundColor: Colors.black,
+        backgroundColor: Colors.deepPurple,
       ),
       body: Container(
         padding: const EdgeInsets.all(10),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Padding(
-              padding: EdgeInsets.fromLTRB(0, 0, 0, 5),
-              child: Text(
-                "Enter Details Below",
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
+            Card(
+              margin: const EdgeInsets.fromLTRB(0, 10, 10, 10),
+              color: Colors.purple,
+              elevation: 5,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20)),
+              child: const Padding(
+                padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                child: Text(
+                  "Enter Details Below",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
                 ),
               ),
             ),
-            Row(
-              children: [
-                const Flexible(
-                    child: TextField(
-                  showCursor: true,
-                  decoration: InputDecoration(
-                    focusColor: Colors.black,
-                    contentPadding: EdgeInsets.all(0),
-                    hintText: "Enter the Task",
-                    fillColor: Colors.black,
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Row(
+                children: [
+                  const Flexible(
+                      child: TextField(
+                    showCursor: true,
+                    decoration: InputDecoration(
+                      focusColor: Colors.black,
+                      contentPadding: EdgeInsets.all(0),
+                      hintText: "Enter the Task",
+                      fillColor: Colors.black,
+                    ),
+                  )),
+                  CustomIconButton(
+                    iconData: Icons.mic,
+                    onPressedFunction: () => {},
                   ),
-                )),
-                CustomIconButton(
-                  iconData: Icons.mic,
-                  onPressedFunction: () => {},
-                ),
-              ],
+                ],
+              ),
             ),
             const SizedBox(
               height: 20,
             ),
-            Row(
-              children: [
-                Flexible(
-                    child: GestureDetector(
-                  child: TextField(
-                    controller: _dateController,
-                    readOnly: true,
-                    showCursor: true,
-                    decoration: const InputDecoration(
-                      focusColor: Colors.black,
-                      contentPadding: EdgeInsets.all(0),
-                      fillColor: Colors.black,
-                      hintText: "Enter Date",
-                    ),
-                  ),
-                )),
-                CustomIconButton(
-                  iconData: Icons.calendar_today,
-                  onPressedFunction: () async {
-                    // ignore: unused_local_variable
-                    DateTime? date = await showDatePicker(
-                        context: context,
-                        initialDate:
-                            (_date == null) ? DateTime.now() : (_date!),
-                        firstDate: DateTime.now(),
-                        lastDate: DateTime(2100));
-                    if (date != null) {
-                      setState(() {
-                        var dateString =
-                            DateFormat('EEEE, d MMM, yyyy').format(date);
-                        _date = date;
-                        _dateController.text = dateString;
-                      });
-                    }
-                  },
-                ),
-                Visibility(
-                  // note:------ visibility false krne par wo element ht jaayega to space bhi nhi lega
-                  visible: (_date == null) ? false : true,
-                  child: CustomIconButton(
-                      iconData: Icons.cancel_rounded,
-                      onPressedFunction: () {
-                        setState(() {
-                          _date = null;
-                          _dateController.text = "";
-                        });
-                      }),
-                ),
-              ],
-            ),
-            Visibility(
-              visible: (_date == null) ? false : true,
+            Padding(
+              padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
               child: Row(
                 children: [
                   Flexible(
-                      child: TextField(
-                    controller: _timeController,
-                    readOnly: true,
-                    showCursor: true,
-                    decoration: const InputDecoration(
-                      focusColor: Colors.black,
-                      contentPadding: EdgeInsets.all(0),
-                      fillColor: Colors.black,
-                      hintText: "Enter Time",
+                      child: GestureDetector(
+                    child: TextField(
+                      controller: _dateController,
+                      readOnly: true,
+                      showCursor: true,
+                      decoration: const InputDecoration(
+                        focusColor: Colors.black,
+                        contentPadding: EdgeInsets.all(0),
+                        fillColor: Colors.black,
+                        hintText: "Enter Date",
+                      ),
                     ),
                   )),
                   CustomIconButton(
-                    iconData: Icons.access_time,
+                    iconData: Icons.calendar_today,
                     onPressedFunction: () async {
                       // ignore: unused_local_variable
-                      TimeOfDay? time = await showTimePicker(
-                        context: context,
-                        initialTime: (_time == null) ? TimeOfDay.now() : _time!,
-                      );
-                      if (time != null) {
+                      DateTime? date = await showDatePicker(
+                          context: context,
+                          initialDate:
+                              (_date == null) ? DateTime.now() : (_date!),
+                          firstDate: DateTime.now(),
+                          lastDate: DateTime(2100));
+                      if (date != null) {
                         setState(() {
-                          _time = time;
-                          var timeString = time.format(context);
-                          _timeController.text = timeString;
+                          var dateString =
+                              DateFormat('EEEE, d MMM, yyyy').format(date);
+                          _date = date;
+                          _dateController.text = dateString;
                         });
                       }
                     },
                   ),
                   Visibility(
                     // note:------ visibility false krne par wo element ht jaayega to space bhi nhi lega
-                    visible: (_time == null) ? false : true,
+                    visible: (_date == null) ? false : true,
                     child: CustomIconButton(
                         iconData: Icons.cancel_rounded,
                         onPressedFunction: () {
                           setState(() {
-                            _time = null;
-                            _timeController.text = "";
+                            _date = null;
+                            _dateController.text = "";
                           });
                         }),
                   ),
                 ],
               ),
             ),
-            const SizedBox(
-              height: 80,
-            ),
             Visibility(
               visible: (_date == null) ? false : true,
-              child: const Padding(
-                padding: EdgeInsets.fromLTRB(0, 0, 0, 5),
-                child: Text(
-                  "Select Repeat Option",
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
-                ),
-              ),
-            ),
-            Visibility(
-              visible: (_date == null) ? false : true,
-              child: DropdownButton<String>(
-                  value: repetitionFrequency,
-                  items: dropDownItemCreator(options),
-                  onChanged: (String? chosenValue) {
-                    setState(() {
-                      if (chosenValue != null) {
-                        if (chosenValue != options.last) {
-                          // options.last="Other"
-                          repetitionFrequency = chosenValue;
-                        } else {
-                          AlertDialog dialog = AlertDialog(
-                            content: const Text("Content"),
-                            actions: [
-                              TextButton(
-                                  onPressed: () {},
-                                  child: const Text("Cancel")),
-                              TextButton(
-                                  onPressed: () {}, child: const Text("OK")),
-                            ],
-                          );
-                          showDialog(
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Flexible(
+                            child: TextField(
+                          controller: _timeController,
+                          readOnly: true,
+                          showCursor: true,
+                          decoration: const InputDecoration(
+                            focusColor: Colors.black,
+                            contentPadding: EdgeInsets.all(0),
+                            fillColor: Colors.black,
+                            hintText: "Enter Time",
+                          ),
+                        )),
+                        CustomIconButton(
+                          iconData: Icons.access_time,
+                          onPressedFunction: () async {
+                            // ignore: unused_local_variable
+                            TimeOfDay? time = await showTimePicker(
                               context: context,
-                              builder: (BuildContext context) {
-                                return dialog;
+                              initialTime:
+                                  (_time == null) ? TimeOfDay.now() : _time!,
+                            );
+                            if (time != null) {
+                              setState(() {
+                                _time = time;
+                                var timeString = time.format(context);
+                                _timeController.text = timeString;
                               });
-                        }
-                      }
-                    });
-                  }),
-            )
+                            }
+                          },
+                        ),
+                        Visibility(
+                          visible: (_time == null) ? false : true,
+                          child: CustomIconButton(
+                              iconData: Icons.cancel_rounded,
+                              onPressedFunction: () {
+                                setState(() {
+                                  _time = null;
+                                  _timeController.text = "";
+                                });
+                              }),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 80,
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.fromLTRB(0, 0, 0, 5),
+                      child: Text(
+                        "Select Repeat Option",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    DropdownButton<String>(
+                        value: repetitionFrequency,
+                        items: dropDownItemCreator(options),
+                        onChanged: (String? chosenValue) {
+                          setState(() {
+                            if (chosenValue != null) {
+                              if (chosenValue != options.last) {
+                                // options.last="Other"
+                                repetitionFrequency = chosenValue;
+                              } else {
+                                AlertDialog dialog = AlertDialog(
+                                  content: const Text("Content"),
+                                  actions: [
+                                    TextButton(
+                                        onPressed: () {},
+                                        child: const Text("Cancel")),
+                                    TextButton(
+                                        onPressed: () {},
+                                        child: const Text("OK")),
+                                  ],
+                                );
+                                showDialog(
+                                    context: context,
+                                    builder: (BuildContext context) {
+                                      return dialog;
+                                    });
+                              }
+                            }
+                          });
+                        }),
+                  ]),
+            ),
           ],
         ),
       ),
     );
+  }
+}
+
+class CustomIconButton extends StatelessWidget {
+  final IconData iconData;
+  final void Function() onPressedFunction;
+  const CustomIconButton({
+    required this.iconData,
+    required this.onPressedFunction,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+        style: TextButton.styleFrom(
+            padding: const EdgeInsets.symmetric(horizontal: 7),
+            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            minimumSize: Size.zero),
+        onPressed: onPressedFunction,
+        child: Icon(
+          iconData,
+          color: Colors.black,
+        ));
   }
 }
