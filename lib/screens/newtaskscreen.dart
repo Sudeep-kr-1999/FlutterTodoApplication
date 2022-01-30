1b9f987e443783469fc3ba92621eb829119bffed
Sudeep Kumar <sudeepagrawal1999@gmail.com>
2022-01-30 16:39:58 +0530

    Icons and Visibility Added in the Add Task Screen

--- lib/screens/newtaskscreen.dart
+++ lib/screens/newtaskscreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:todoapplication/CustomIcon.dart';
 
 class NewTaskScreen extends StatefulWidget {
   const NewTaskScreen({Key? key}) : super(key: key);
@@ -8,9 +10,11 @@ class NewTaskScreen extends StatefulWidget {
 }
 
 class _NewTaskScreenState extends State<NewTaskScreen> {
-  String taskName="";
+  String taskName = "";
   DateTime? _date;
+  TimeOfDay? _time;
   final TextEditingController _dateController = TextEditingController();
+  final TextEditingController _timeController = TextEditingController();
   String repetitionFrequency = "No Repeat";
   List<String> options = [
     "No Repeat",
@@ -100,8 +104,10 @@ class _NewTaskScreenState extends State<NewTaskScreen> {
                         lastDate: DateTime(2100));
                     if (date != null) {
                       setState(() {
+                        var dateString =
+                            DateFormat('EEEE, d MMM, yyyy').format(date);
                         _date = date;
-                        _dateController.text = _date.toString();
+                        _dateController.text = dateString;
                       });
                     }
                   },
@@ -120,121 +126,106 @@ class _NewTaskScreenState extends State<NewTaskScreen> {
                 ),
               ],
             ),
-            Row(
-              children: [
-                Flexible(
-                    child: TextField(
-                  controller: _dateController,
-                  readOnly: true,
-                  showCursor: true,
-                  decoration: const InputDecoration(
-                    focusColor: Colors.black,
-                    contentPadding: EdgeInsets.all(0),
-                    fillColor: Colors.black,
-                    hintText: "Enter Date",
-                  ),
-                )),
-                CustomIconButton(
-                  iconData: Icons.lock_clock_rounded,
-                  onPressedFunction: () async {
-                    // ignore: unused_local_variable
-                    DateTime? date = await showDatePicker(
+            Visibility(
+              visible: (_date == null) ? false : true,
+              child: Row(
+                children: [
+                  Flexible(
+                      child: TextField(
+                    controller: _timeController,
+                    readOnly: true,
+                    showCursor: true,
+                    decoration: const InputDecoration(
+                      focusColor: Colors.black,
+                      contentPadding: EdgeInsets.all(0),
+                      fillColor: Colors.black,
+                      hintText: "Enter Time",
+                    ),
+                  )),
+                  CustomIconButton(
+                    iconData: Icons.access_time,
+                    onPressedFunction: () async {
+                      // ignore: unused_local_variable
+                      TimeOfDay? time = await showTimePicker(
                         context: context,
-                        initialDate:
-                            (_date == null) ? DateTime.now() : (_date!),
-                        firstDate: DateTime.now(),
-                        lastDate: DateTime(2100));
-                    if (date != null) {
-                      setState(() {
-                        _date = date;
-                        _dateController.text = _date.toString();
-                      });
-                    }
-                  },
-                ),
-                Visibility(
-                  // note:------ visibility false krne par wo element ht jaayega to space bhi nhi lega
-                  visible: (_date == null) ? false : true,
-                  child: CustomIconButton(
-                      iconData: Icons.cancel_rounded,
-                      onPressedFunction: () {
+                        initialTime: (_time == null) ? TimeOfDay.now() : _time!,
+                      );
+                      if (time != null) {
                         setState(() {
-                          _date = null;
-                          _dateController.text = "";
+                          _time = time;
+                          var timeString = time.format(context);
+                          _timeController.text = timeString;
                         });
-                      }),
-                ),
-              ],
+                      }
+                    },
+                  ),
+                  Visibility(
+                    // note:------ visibility false krne par wo element ht jaayega to space bhi nhi lega
+                    visible: (_time == null) ? false : true,
+                    child: CustomIconButton(
+                        iconData: Icons.cancel_rounded,
+                        onPressedFunction: () {
+                          setState(() {
+                            _time = null;
+                            _timeController.text = "";
+                          });
+                        }),
+                  ),
+                ],
+              ),
             ),
             const SizedBox(
               height: 80,
             ),
-            const Padding(
-              padding: EdgeInsets.fromLTRB(0, 0, 0, 5),
-              child: Text(
-                "Select Repeat Option",
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
+            Visibility(
+              visible: (_date == null) ? false : true,
+              child: const Padding(
+                padding: EdgeInsets.fromLTRB(0, 0, 0, 5),
+                child: Text(
+                  "Select Repeat Option",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
                 ),
               ),
             ),
-            DropdownButton<String>(
-                value: repetitionFrequency,
-                items: dropDownItemCreator(options),
-                onChanged: (String? chosenValue) {
-                  setState(() {
-                    if (chosenValue != null) {
-                      if (chosenValue != options.last) {
-                        // options.last="Other"
-                        repetitionFrequency = chosenValue;
-                      } else {
-                        AlertDialog dialog = AlertDialog(
-                          content: const Text("Content"),
-                          actions: [
-                            TextButton(
-                                onPressed: () {}, child: const Text("Cancel")),
-                            TextButton(
-                                onPressed: () {}, child: const Text("OK")),
-                          ],
-                        );
-                        showDialog(
-                            context: context,
-                            builder: (BuildContext context) {
-                              return dialog;
-                            });
+            Visibility(
+              visible: (_date == null) ? false : true,
+              child: DropdownButton<String>(
+                  value: repetitionFrequency,
+                  items: dropDownItemCreator(options),
+                  onChanged: (String? chosenValue) {
+                    setState(() {
+                      if (chosenValue != null) {
+                        if (chosenValue != options.last) {
+                          // options.last="Other"
+                          repetitionFrequency = chosenValue;
+                        } else {
+                          AlertDialog dialog = AlertDialog(
+                            content: const Text("Content"),
+                            actions: [
+                              TextButton(
+                                  onPressed: () {},
+                                  child: const Text("Cancel")),
+                              TextButton(
+                                  onPressed: () {}, child: const Text("OK")),
+                            ],
+                          );
+                          showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return dialog;
+                              });
+                        }
                       }
-                    }
-                  });
-                })
+                    });
+                  }),
+            )
           ],
         ),
       ),
     );
-  }
-}
-
-class CustomIconButton extends StatelessWidget {
-  final IconData iconData;
-  final void Function() onPressedFunction;
-  const CustomIconButton({
-    required this.iconData,
-    required this.onPressedFunction,
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return TextButton(
-        style: TextButton.styleFrom(
-            padding: const EdgeInsets.symmetric(horizontal: 7),
-            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-            minimumSize: Size.zero),
-        onPressed: onPressedFunction,
-        child: Icon(
-          iconData,
-          color: Colors.black,
-        ));
   }
 }
--- lib/task.dart
+++ lib/task.dart
@@ -1,80 +1,126 @@
-enum RepeatCycle {
-  onceADay,
-  onceADayMonFri,
-  onceAWeek,
-  onceAMonth,
-  other,
-}
-enum Tenure {
-  days,
-  weeks,
-  months,
-  years,
-}
+// enum RepeatCycle {
+//   onceADay,
+//   onceADayMonFri,
+//   onceAWeek,
+//   onceAMonth,
+//   onceAYear,
+//   other,
+// }
+// enum Tenure { days, weeks, months, years }
 
-class RepeatFrequency {
-  int num;
-  Tenure tenure;
-  RepeatFrequency({required this.num, required this.tenure});
-}
+// class RepeatFrequency {
+//   RepeatFrequency({required this.num, required this.tenure});
+//   int num;
+//   Tenure tenure;
+// }
 
-class Task {
-  String taskListid;
-  String taskId;
-  // used for repeated task only
-  String? parentTasktaskId;
-  String taskName = "";
-  DateTime? deadLineDate;
-  DateTime? deadLineTime;
-  bool finished;
-  bool skipped;
-  Task({
-    required this.taskListid,
-    required this.taskId,
-    required this.taskName,
-    required this.finished,
-    required this.skipped,
-    this.deadLineDate,
-    this.deadLineTime,
-    this.parentTasktaskId,
-  }) {
-    if (deadLineDate == null) {
-      // if condition false it will show the error
-      assert(deadLineTime == null);
-    }
-  }
-}
+// class Task {
+//   static late int counter;
+//   Task({
+//     required this.taskName,
+//     required this.finished,
+//     required this.taskListID,
+//     taskId = null,
+//     this.parentTaskID,
+//     this.deadlineDate,
+//     this.deadlineTime,
+//   }) {
+//     if (taskId == null) {
+//       this.taskId = counter;
+//       counter++;
+//     } else {
+//       this.taskId = taskId;
+//     }
+//   }
 
-class RepeatingTask {
-  String taskListid;
-  String repeatedTaskId;
-  String repeatingTaskName;
-  RepeatCycle repeatCycle;
-  RepeatFrequency? repeatFrequency;
-  DateTime? deadLineTime;
-  DateTime deadLineDate;
-  RepeatingTask({
-    required this.taskListid,
-    required this.repeatedTaskId,
-    required this.repeatingTaskName,
-    required this.repeatCycle,
-    this.repeatFrequency,
-    required this.deadLineDate,
-    this.deadLineTime,
-  });
-}
+//   static initializeCounter(int counter) {
+//     Task.counter = counter;
+//   }
 
-class TaskList {
-  String taskListId;
-  String taskListName;
-  List<Task> nonRepeatingTask = [];
-  List<RepeatingTask> repeatingTask = [];
-  List<Task> repeatingTaskInstance = [];
-  TaskList({
-    required this.taskListId,
-    required this.taskListName,
-    required this.nonRepeatingTask,
-    required this.repeatingTask,
-    required this.repeatingTaskInstance,
-  });
-}
+//   late int taskId;
+//   int taskListID;
+//   int? parentTaskID; //used for repeated task instances only
+//   String taskName;
+//   DateTime? deadlineDate;
+//   DateTime? deadlineTime;
+//   bool finished;
+//   void finishTask() {
+//     finished = true;
+//   }
+// }
+
+// class RepeatingTask {
+//   RepeatingTask({
+//     required this.repeatingTaskId,
+//     required this.repeatingTaskName,
+//     required this.repeatCycle,
+//     required this.deadlineDate,
+//     this.repeatFrequency,
+//     this.deadlineTime,
+//     required this.taskListID,
+//   });
+//   int taskListID;
+//   int repeatingTaskId;
+//   String repeatingTaskName;
+//   RepeatCycle repeatCycle;
+//   RepeatFrequency? repeatFrequency;
+//   DateTime deadlineDate;
+//   DateTime? deadlineTime;
+// }
+
+// class TaskList {
+//   int taskListID;
+//   String taskListName;
+//   List<Task> nonRepeatingTasks;
+//   List<RepeatingTask> repeatingTasks;
+//   List<Task> activeRepeatingTaskInstances;
+//   TaskList({
+//     required this.nonRepeatingTasks,
+//     required this.repeatingTasks,
+//     required this.activeRepeatingTaskInstances,
+//     required this.taskListID,
+//     required this.taskListName,
+//   });
+//   List<Task> getActiveTasks() {
+//     //TODO::Select repeating Task Instances as well
+//     List<Task> activeNonRepeatingTasks = [];
+//     {
+//       for (var i = 0; i < nonRepeatingTasks.length; i++) {
+//         if (nonRepeatingTasks[i].finished == false) {
+//           activeNonRepeatingTasks.add(nonRepeatingTasks[i]);
+//         }
+//       }
+//       return (activeNonRepeatingTasks);
+//     }
+//   }
+
+//   List<Task> getFinishedTasks() {
+//     //repeating Instances as well as non-repeating Instances
+//     return ([]);
+//   }
+
+//   void FinishTask(Task task) {}
+
+//   void addTask({
+//     required String taskName,
+//     DateTime? deadlineDate,
+//     DateTime? deadlineTime,
+//     int? parentTaskID,
+//   }) {
+//     //
+//     Task(
+//       taskName: taskName,
+//       finished: false,
+//       taskListID: taskListID,
+//       deadlineDate: deadlineDate,
+//       deadlineTime: deadlineTime,
+//       parentTaskID: parentTaskID,
+//     );
+
+//     if (parentTaskID != null) {
+//       //
+//     }
+//   }
+
+//   void finishTask(Task task) {}
+// }
